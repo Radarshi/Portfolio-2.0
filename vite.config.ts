f6ab98c1925d7a0160926e0b4ac4555d76217e00
Radarshi Mukherjee <radarshim2@gmail.com>
2025-07-10 01:26:29 +0530

    Jai mata di

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import { componentTagger } from "lovable-tagger";
 import path from "path";
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base : "./",
   server: {
     host: "::",
     port: 8080,
