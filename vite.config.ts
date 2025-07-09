17b4d5580bb26db37c1233f48c79dc90eb2e73b6
Radarshi Mukherjee <radarshim2@gmail.com>
2025-07-10 00:58:18 +0530

    Changed Packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { componentTagger } from "lovable-tagger";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
